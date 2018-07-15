b901036bd19366b0dad2b893ab83c17fdea8478e
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-07-15 13:23:38 +0300

    Add tests for Category Card

--- tests/components/categories/CategoryCard.test.jsx
+++ tests/components/categories/CategoryCard.test.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import CategoryCard from '../../../src/components/categories/CategoryCard';
 import categories from '../../../src/fixtures/categories';
 
 describe('<CategoryCard />', () => {
+  const props = {
+    page: '/u/categories',
+  };
   const wrapper = mount.bind(
     null,
-    <CategoryCard {...categories} />,
+    <CategoryCard {...categories[0]} />,
   );
 
   it('should render without crashing', () => {
     expect(wrapper).not.toThrowError();
+  });
+
+  let shallowWrapper;
+
+  beforeEach(() => {
+    shallowWrapper = shallow(<CategoryCard {...props} {...categories[0]} />);
+  });
+
+  it('should render at least a description', () => {
+    expect(wrapper().find('.activity__description').length).toBe(1);
+  });
+
+  it('should render category details', () => {
+    expect(wrapper().find('.category').length).toBe(1);
+  });
+
+  it('should render delete button on categories page', () => {
+    expect(shallowWrapper.find('.deleteCategory__button').length).toBe(1);
+  });
+
+  it('should render check box on categories page cards', () => {
+    expect(shallowWrapper.find('.category__checkbox').length).toBe(1);
+  });
+
+  it('should change state when a checkbox of a category is clicked', () => {
+    shallowWrapper.setState({ isCategoryChecked: true });
+    const checkbox = shallowWrapper.find('.category__checkbox');
+    checkbox.simulate('change');
+    expect(shallowWrapper.state().isCategoryChecked).toBe(false);
   });
 });
